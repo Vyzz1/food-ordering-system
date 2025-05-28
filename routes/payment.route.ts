5c1ded73efa26a77d41eae79c4d61345099d0d7b
Huỳnh Khang Vỹ <52200024@student.tdtu.edu.vn>
2025-05-28 11:21:43 +0700

    feat: enhance webhook route to support raw JSON payloads

--- routes/payment.route.ts
+++ routes/payment.route.ts
@@ -1,8 +1,14 @@
-import { Router } from "express";
+import express, { Router } from "express";
 import paymentController from "../controllers/payment.controller";
 
 const paymnetRoute = Router();
 
-paymnetRoute.post("/webhook", paymentController.webHookHanlder);
+paymnetRoute.post(
+  "/webhook",
+  express.raw({
+    type: "application/json",
+  }),
+  paymentController.webHookHanlder
+);
 
 export default paymnetRoute;
