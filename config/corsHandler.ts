302158d451eb631423b248901e429adf55073acc
Huỳnh Khang Vỹ <52200024@student.tdtu.edu.vn>
2025-05-28 22:35:56 +0700

    feat: update CORS configuration to use the correct production domain

--- config/corsHandler.ts
+++ config/corsHandler.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 
 const corsHandler = cors({
-  origin: ["http://localhost:3000", "https://your-production-domain.com"],
+  origin: ["http://localhost:3000", "https://foodyx-kappa.vercel.app"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
